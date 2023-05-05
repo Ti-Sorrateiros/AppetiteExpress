f980d98ca621e0ba1b0f9acb52b4a927ef8f77d7
joaoschrock <joaoschrock@gmail.com>
2023-05-04 21:19:48 -0300

    Created Menu.JS

---
+++ src/js/menu.js
@@ -0,0 +1,21 @@
+var menuItem =  document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+//Area de expansao do menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
