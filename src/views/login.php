1202667653109ca21ba7cf6dd1d01193d7a10ba4
gladsonssimoes <gladsonssimoes@outlook.com.br>
2023-05-06 19:25:56 -0300

    Página de Login

---
+++ src/views/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div align="center">
+        <h1>Login</h1>
+        <h3>Faça seu Login</h3>
+        <form method="post" action="">
+            <div>
+                <input type="email" name="email" placeholder="E-mail">
+            </div>
+            <br>
+            <div>
+                <input type="password" name="senha" placeholder="Digite seu senha">
+            </div>
+            <br>
+            <div>
+                <button type="submit" name="loginUser">Logar</button>
+            </div>
+        </form>
+            <div>
+                <p>Não tem uma conta? </p>
+                <a href="cadastro">Clique para fazer seu cadastro</a>
+            </div>
+        
+
+    </div>
+</body>
+
+</html>
