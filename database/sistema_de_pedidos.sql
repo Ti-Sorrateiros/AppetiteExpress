8e4f14d9bae83ea2fea8e71fded3898a3c744b2f
gladsonssimoes <gladsonssimoes@outlook.com.br>
2023-05-06 15:36:36 -0300

    mudança no banco de dados

--- database/sistema_de_pedidos.sql
+++ database/sistema_de_pedidos.sql
@@ -1,27 +1,24 @@
-CREATE DATABASE sistema_de_pedidos;
+CREATE DATABASE if not exists sistema_de_pedidos;
 USE sistema_de_pedidos;
 
-create table usuarios(
+create table if not exists usuarios(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_perfil INT 
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(100) NOT NULL,
     telefone VARCHAR(11) NOT NULL,
-    endereço 
-);
+    endereco varchar(50) NOT NULL,
+    senha VARCHAR(100) NOT NULL
+)  default charset utf8;
 
-
-create table produtos(
+create table if not exists produtos(
     id INT PRIMARY KEY AUTO_INCREMENT,
     saborPizza VARCHAR(50) NOT NULL
-);
+) default charset utf8;
 
-
-create table pedidos(
+create table if not exists pedidos(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nomePedido VARCHAR(55) NOT NULL
-);
+) default charset utf8;
 
 
 
