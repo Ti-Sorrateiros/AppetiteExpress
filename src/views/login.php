f7b5fe5038abed7a6f54fcfd8bf5dd4a3b2cd4dc
JoaoSchrock <101228590+SchrockHolmes@users.noreply.github.com>
2023-05-06 23:42:57 -0300

    Inicio da stylization  do Login

--- src/views/login.php
+++ src/views/login.php
@@ -5,31 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/login.css">
+    <link rel="stylesheet" href="../styles/GoogleFonts/GoogleFonts.css">
+    <!-- <link rel="stylesheet" href="../styles/bootstrap/Bootstrap@5.2.3.css"> -->
     <title>Login</title>
 </head>
 
 <body>
-    <div align="center">
-        <h1>Login</h1>
-        <h3>Faça seu Login</h3>
+    <div class="container" align="center">
+            <h1 id="TLogin">Faça seu Login</h1>
+        <h3 id="SLogin">Informe um login valido</h3>
         <form method="post" action="">
-            <div>
-                <input type="email" name="email" placeholder="E-mail">
+                <div class="form__group field">
+                <input type="email" class="form__field" placeholder="Name" required="">
+                <label for="email" class="form__label">E-mail:</label>
+            </div>
+            <br>
+            <div class="Senha">
+                <div class="form__group field">
+                <input type="password" class="form__field" placeholder="Name" required="">
+                <label for="password" class="form__label">Senha:</label>
+            </div>
             </div>
             <br>
             <div>
-                <input type="password" name="senha" placeholder="Digite seu senha">
-            </div>
-            <br>
-            <div>
-                <button type="submit" name="loginUser">Logar</button>
+                <button id="Lbutton" type="submit" onclick="" name="loginUser">ENTRAR</button>
             </div>
         </form>
             <div>
-                <p>Não tem uma conta? </p>
-                <a href="cadastro">Clique para fazer seu cadastro</a>
+                <p id="Cadastrar-se">Ainda não é cadastrado? <a href="cadastro" style="color:#73BB18;">Cadastre-se</a></p>
             </div>
-        
 
     </div>
 </body>
