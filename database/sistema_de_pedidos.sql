2655142db3e33845b485ae4d551e1562dfdaecb5
jnovais1 <109621297+jnovais1@users.noreply.github.com>
2023-05-08 21:13:10 -0300

    Dump do banco de dados

--- database/sistema_de_pedidos.sql
+++ database/sistema_de_pedidos.sql
@@ -1,25 +1,62 @@
-CREATE DATABASE if not exists sistema_de_pedidos;
+CREATE DATABASE sistema_de_pedidos;
 USE sistema_de_pedidos;
 
-create table if not exists usuarios(
+CREATE TABLE usuarios(
     id INT PRIMARY KEY AUTO_INCREMENT,
+    id_perfil INT, 
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    telefone VARCHAR(11) NOT NULL,
-    endereco varchar(50) NOT NULL,
-    senha VARCHAR(100) NOT NULL
-)  default charset utf8;
+    senha VARCHAR(100) NOT NULL,
+    telefone VARCHAR(11) NOT NULL UNIQUE,
+    endereço VARCHAR(50) NOT NULL
+) DEFAULT charset utf8; 
 
-create table if not exists produtos(
+CREATE TABLE perfil(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    saborPizza VARCHAR(50) NOT NULL
-) default charset utf8;
+    tipo_de_usuario VARCHAR(25) NOT NULL
+) DEFAULT charset utf8;
 
-create table if not exists pedidos(
+CREATE TABLE produtos(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nomePedido VARCHAR(55) NOT NULL
-) default charset utf8;
+    nome VARCHAR(50) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    preco FLOAT(15) NOT NULL,
+    imagem VARCHAR(255),
+    adicionais VARCHAR(255)
+) DEFAULT charset utf8; 
 
 
+CREATE TABLE pedidos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    id_carrinho INT,
+    id_mesa INT,
+    valor_pedido FLOAT(15) NOT NULL,
+    tipo_pedido VARCHAR(55) NOT NULL,
+    data_pedido DATETIME NOT NULL
+) DEFAULT charset utf8; 
 
+CREATE TABLE mesas(
+    id INT PRIMARY KEY,
+    status VARCHAR(55) NOT NULL,
+    disponibilidade VARCHAR(25) NOT NULL
+) DEFAULT charset utf8; 
 
+CREATE TABLE carrinho(
+    id INT PRIMARY KEY,
+    id_produto INT,
+    quantidade_produto INT(10)
+) DEFAULT charset utf8; 
+
+ALTER TABLE usuarios ADD FOREIGN KEY(id_perfil) REFERENCES perfil (id);
+
+ALTER TABLE pedidos ADD FOREIGN KEY(id_cliente) REFERENCES usuarios (id);
+
+ALTER TABLE pedidos ADD FOREIGN KEY(id_carrinho) REFERENCES carrinho (id);
+
+ALTER TABLE pedidos ADD FOREIGN KEY(id_mesa) REFERENCES mesas (id);   
+
+ALTER TABLE carrinho ADD FOREIGN KEY(id_produto) REFERENCES produtos (id);
+
+INSERT INTO perfil (tipo_de_usuario) VALUES ('admin');
+INSERT INTO perfil (tipo_de_usuario) VALUES ('common');
