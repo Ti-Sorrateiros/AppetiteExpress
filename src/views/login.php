4377d041b75b65eaba8d06bf806fd606feae57ff
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-05-15 15:43:45 -0300

    ajustes no formulario login

--- src/views/login.php
+++ src/views/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="../styles/Lvalidation.css">
+    <link rel="stylesheet" href="../styles/Lvalidation.css">
     <link rel="stylesheet" href="../styles/login.css">
     <link rel="stylesheet" href="../styles/GoogleFonts/GoogleFonts.css">
     <!-- <link rel="stylesheet" href="../styles/bootstrap/Bootstrap@5.2.3.css"> -->
@@ -14,15 +14,15 @@
 
 <body>
     <div class="container">
-        
+
         <h1 id="TLogin">Faça seu Login</h1>
         <h3 id="SLogin">Informe um login válido</h3>
-<form class="form" id="form">
+        <form method="post" action="../controllers/user/userController.php" >
             <div class="form-control">
                 <label>Email</label>
-                <input type="email" id="email">
-                <i class="img-success" ><img src="../images/success-icon.svg" alt=""></i>
-                <i class="img-error" ><img src="../images/error-icon.svg" alt=""></i>
+                <input type="email" id="email" name="email">
+                <i class="img-success"><img src="../images/success-icon.svg" alt=""></i>
+                <i class="img-error"><img src="../images/error-icon.svg" alt=""></i>
                 <small id="msg-error">Error Message</small>
                 <i class="img-alert"><img src="../images/alert-icon.svg" alt=""></i>
                 <small id="msg-alert">Login não Encontrado</small>
@@ -31,23 +31,18 @@
             </div>
 
             <div class="form-control">
-
                 <label>Senha</label>
-                <input type="password" id="password">
-                <i class="img-success" ><img src="../images/success-icon.svg" alt=""></i>
-                <i class="img-error" ><img src="../images/error-icon.svg" alt=""></i>
+                <input type="password" id="password" name="senha" />
+                <i class="img-success"><img src="../images/success-icon.svg" alt=""></i>
+                <i class="img-error"><img src="../images/error-icon.svg" alt=""></i>
                 <small id="msg-error">Error Message</small>
                 <i class="img-alert"><img src="../images/alert-icon.svg" alt=""></i>
                 <small id="msg-alert">Login não Encontrado</small>
             </div>
 
-            <button type="submit" >ENTRAR</button>
+            <button type="submit" name="loginUser">ENTRAR</button>
+     
         </form>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
     </div>
     <script src="../js/Lvalidation.js"></script>
 </body>
