8eeea9b295d92f7c66467ecbfd748c3ff40158fa
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-05-22 10:46:26 -0300

    teste da tela produtos

--- src/views/produtos.php
+++ src/views/produtos.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/RealizarPedido.css">
     <link rel="stylesheet" href="../styles/menu.css">
-    <link rel="stylesheet" href="../styles/content.css">
     <link rel="stylesheet" href="../styles/GoogleFonts/GoogleFonts.css">
+    <link rel="stylesheet" href="../styles/content.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <title>Sistema Pedido - Produtos</title>
+    <title>Carrinho </title>
 </head>
 
 <body>
@@ -54,10 +55,52 @@
         </ul>
     </nav>
 
-    <div class="content">
-        <p>Página de Produtos</p>
-    </div>
+    
+        <div class="content">
+            <h1>Produtos</h1>
+            <p>Escolha seus produtos</p>
+            <br>
+            <div>
+                <?php
+                $items = array
+                (
+                    ['imagem' => '../images/products/hamburguer.jpg', 'preco' => '200'],
+                    ['imagem' => '../images/products/pizza.jpg', 'preco' => '100'],
+                    ['nome' => 'Curso 3', 'preco' => '400']
+                );
 
+                foreach ($items as $key => $value) {
+                    ?>
+                    <!-- produto -->
+                    <div class="produto">
+                        <img src="<?php echo $value['imagem'] ?>">
+                        <a href="?adicionar=<?php echo $key ?>">Adicionar ao carrinho!</a>
+                    </div>
+
+                    <?php
+                }
+                ?>
+                <div></div>
+                <?php
+                if (isset($_GET['adicionar'])) {
+                    //vamos adicionar ao carrinho.
+                    $idProduto = (int) $_GET['adicionar'];
+                    if (isset($items[$idProduto])) {
+                        if (isset($_SESSION[$idProduto])) {
+                            $_SESSION[$idProduto]['quantidade']++;
+                        } else {
+                            $_SESSION[$idProduto] = array('quantidade' => 1, 'nome' => $items[$idProduto]['nome'], 'preco' => $items[$idProduto]['preco']);
+                        }
+                        echo '<script>alert("o item foi adicionado ao carrinho");</script>';
+                    } else {
+                        die('Você não pode adicionar um item que não existe.');
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    
+        
     <script src="../js/menu.js" type="text/javascript"></script>
     <script src="../js/noRefresh.js" type="text/javascript"></script>
 </body>
