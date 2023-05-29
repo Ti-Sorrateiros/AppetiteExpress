b48f3baf0769e18effc0a70f5f32884d7395e02a
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-05-29 14:13:55 -0300

    tela para gerenciar os pedidos feitos

---
+++ src/views/admin/pedidosFeitos.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/RealizarPedido.css">
+    <link rel="stylesheet" href="../../styles/menu.css">
+    <link rel="stylesheet" href="../../styles/GoogleFonts/GoogleFonts.css">
+    <link rel="stylesheet" href="../../styles/content.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <title>Cadastrar Produtos</title>
+</head>
+
+<body>
+    <nav class="menu-lateral">
+
+        <div class="btn-expandir">
+            <i class="bi bi-list" id="btn-exp"></i>
+        </div>
+        <ul>
+            <li class="item-menu">
+                <a href="cadastrarProdutos">
+                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
+                    <span class="txt-link"> CadastrarProdutos</span>
+                </a>
+            </li>
+            <li class="item-menu">
+                <a href="pedidosFeitos">
+                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
+                    <span class="txt-link"> PedidosFeitos </span>
+                </a>
+            </li>
+            <li class="item-menu">
+                <a href="usuarios">
+                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
+                    <span class="txt-link"> Usuarios</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="content">
+        <h1>Pedidos Feitos</h1>
+    </div>
+
+    <script src="../../js/menu.js" type="text/javascript"></script>
+    <script src="../../js/noRefresh.js" type="text/javascript"></script>
+</body>
+
+</html>
