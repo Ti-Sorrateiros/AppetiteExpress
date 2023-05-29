5e5bc6b0f6e9a73b049bc3b221ff494add915630
juliandersonsoares <jsoares.4720@gmail.com>
2023-05-29 20:59:23 -0300

    pagina de listar produtos

---
+++ src/views/listaProduto.php
@@ -0,0 +1,38 @@
+<?php
+include('../controler/conn.php');
+$tabela = $conn->prepare("SELECT * FROM cadastroproduto");
+$tabela->execute();
+$rowTabela = $tabela->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <tbody>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Descrição</td>
+                <td>Preço</td>
+            </tr>
+        <?php
+        foreach($rowTabela as $linha){
+            echo '<tr>';
+            echo "<th>".$linha['id']."</th>";
+            echo "<td>".$linha['descricao']."</td>";
+            echo "<td>".$linha['preco']."</td>";
+            echo '<td><a href=../controllercadastro.php?mensagem='.$linha['id'].'>Editar</a></td>';
+            echo '<td><a href=../controllercadastro.php?mensagem='.$linha['id'].'>Excluir</a></td>';
+            echo '</tr>';
+        }
+        ?>
+    </tbody>
+    </table>
+</body>
+</html>
