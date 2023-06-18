8362ef874da29399a1e88ce6647aa55238bb5e13
JoaoSchrock <101228590+SchrockHolmes@users.noreply.github.com>
2023-06-18 15:18:55 -0300

    Redirecionei de cadastro para login

--- src/views/cadastro.php
+++ src/views/cadastro.php
@@ -78,7 +78,7 @@ include('../../database/conn.php')
                 
             <button type="submit" onclick="" name="createUser">CADASTRAR</button>
            
-            <p id="Cadastrar-se">Já possui Login? <a href="cadastro" style="color:#a29df3;">Entrar</a></p>
+            <p id="Cadastrar-se">Já possui Login? <a href="login.php" style="color:#a29df3;">Entrar</a></p>
 
         </form>
         
