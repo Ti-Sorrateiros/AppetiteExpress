7a9a0cc06b7a2d1a27d6811f7bc570b1790c82e3
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-06-27 21:48:23 -0300

    backend para atualizar produtos

--- src/controllers/products/productController.php
+++ src/controllers/products/productController.php
@@ -5,16 +5,15 @@ $product = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 if(isset($product['createProduto'])){
     createProduct($product);
-}
+} else if (isset($product['updateProduto'])){
+    updateProduct($product);
+} 
 
 function createProduct($product)
 {
     global $conn;
 
-    $cadastroProduto = $conn->prepare("INSERT INTO produtos
-    (nome, descricao, preco, adicionais) 
-    VALUES
-    (:nome,:descricao,:preco,:adicionais)");
+    $cadastroProduto = $conn->prepare("INSERT INTO produtos (nome, descricao, preco, adicionais) VALUES (:nome, :descricao, :preco, :adicionais)");
     $cadastroProduto->execute(array(
     ':nome'=>$product['nome'],
     ':descricao'=>$product['descricao'],
@@ -35,6 +34,41 @@ function createProduct($product)
     //      window.location.href='../../views/admin/cadastrarProdutos.php';
     //     </script>");
     // }
+
+}
+
+function updateProduct($product)
+{
+    global $conn;
+
+    $updateProduto = $conn->prepare("UPDATE produtos set :nome , :descricao , :preco, :adicionais WHERE id= :id");
+    $updateProduto->execute(array(
+    ':nome'=>$product['nome'],
+    ':descricao'=>$product['descricao'],
+    ':preco'=>$product['preco'],
+    ':adicionais'=>$product['adicionais'],
+    ':id'=>$product['id']
+    ));
+    
+    if($updateProduto == true){
+        echo ("<script>
+         alert('Produto Foi Editado com Sucesso');
+         window.location.href='../../views/admin/cadastrarProdutos.php';
+        </script>");
+    } 
+    //precisa fazer o exception para erro
+    // else {
+    //     echo ("<script>
+    //      alert('ERRO: NAO FOI ATUALIZADO');
+    //      window.location.href='../../views/admin/cadastrarProdutos.php';
+    //     </script>");
+    // }
+
+}
+
+//adicionar delete!
+function deleteProduct($product)
+{
 
 }
 
