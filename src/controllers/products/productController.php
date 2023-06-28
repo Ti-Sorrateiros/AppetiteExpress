2ef75d2a03f74ba4b57c46c3febc7a7ab9258539
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-06-27 21:08:14 -0300

    fazendo o cadastro do Produto

--- src/controllers/products/productController.php
+++ src/controllers/products/productController.php
@@ -1,12 +1,42 @@
 <?php
-include('conn.php');
-$nome = $_POST['nome'];
-$descricao=$_POST['descricao'];
-$preco=$_POST['preco'];
-$imagem=$_POST[''];
-$adicionais = $_POST['adicionais'];
+include('../../../database/conn.php');
 
-$cds = $conn->prepare("INSERT INTO produtos (nome,descricao,preco,imagem,adicionais) VALUES(:nome,:descricao,:preco,:imagem,:adicionais)");
-$cadastro->execute(array(':nome'=>$nome,':descricao'=>$descricao, ':preco'=>$preco, ':imagem'=>$imagem, ':adicionais'=>$adicionais));
+$product = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+if(isset($product['createProduto'])){
+    createProduct($product);
+}
+
+function createProduct($product)
+{
+    global $conn;
+
+    $cadastroProduto = $conn->prepare("INSERT INTO produtos
+    (nome, descricao, preco, adicionais) 
+    VALUES
+    (:nome,:descricao,:preco,:adicionais)");
+    $cadastroProduto->execute(array(
+    ':nome'=>$product['nome'],
+    ':descricao'=>$product['descricao'],
+    ':preco'=>$product['preco'],
+    ':adicionais'=>$product['adicionais']
+    ));
+    
+    if($cadastroProduto == true){
+        echo ("<script>
+         alert('Produto Foi Cadastrado com Sucesso');
+         window.location.href='../../views/admin/cadastrarProdutos.php';
+        </script>");
+    } 
+    //precisa fazer o exception para erro
+    // else {
+    //     echo ("<script>
+    //      alert('ERRO: Produto NAO FOI CADASTRO');
+    //      window.location.href='../../views/admin/cadastrarProdutos.php';
+    //     </script>");
+    // }
+
+}
+
 
 ?>
--- src/views/admin/cadastrarProdutos.php
+++ src/views/admin/cadastrarProdutos.php
@@ -15,6 +15,32 @@
 </head>
 
 <body>
+    <div class="content">
+        <div class="container">
+            <h1>Cadastrar Produtos</h1>
+            <form action="../../controllers/products/productController.php" method="post">
+                <label>Nome do Produto<input type="text" name="nome" required></label>
+                <label>Descrição <input type="text" name="descricao" required></label>
+                <label>Preço <input type="text" name="preco" required></label>
+                <label>Adicionais <input type="text" name="adicionais" required></label>
+                <!-- <label>Imagem <input type="file" required></label> -->
+                <button type="submit" name="createProduto">Cadastrar</button>
+            </form>
+        </div>
+
+        <div class="desconto">
+            <h1>Inserir Desconto</h1>
+            <form action="../../controller/controllercadastro.php" method="post">
+                <label>ID do Produto <input type="text" name="id_product" required></label>
+                <label>Desconto<input type="text" name="desconto" required></label>
+                <button type="submit">INSERIR DESCONTO</button>
+            </form>
+        </div>
+    </div>
+
+
+
+
     <nav class="menu-lateral">
 
         <div class="btn-expandir">
@@ -26,56 +52,27 @@
                     <span class="icon"><i class="bi bi-bag-fill"></i></span>
                     <span class="txt-link"> CadastrarProdutos</span>
                 </a>
-                </div>
             </li>
             <li class="item-menu">
                 <a href="pedidosFeitos">
                     <span class="icon"><i class="bi bi-bag-fill"></i></span>
-                    <span class="txt-link"> PedidosFeitos </span>
+                    <span class="txt-link"> PedidosFeitos</span>
                 </a>
-                </div>
             </li>
             <li class="item-menu">
                 <a href="usuarios">
                     <span class="icon"><i class="bi bi-bag-fill"></i></span>
                     <span class="txt-link"> Usuarios</span>
                 </a>
-                </div>
             </li>
-
+            <li class="item-menu">
+                <a href="verprodutos">
+                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
+                    <span class="txt-link">VerProdutos</span>
+                </a>
+            </li>
         </ul>
     </nav>
-
-    <div class="content">
-        
-        <div class="container">
-        <h1>Cadastrar Produtos</h1>
-            <form action="../../controller/controllercadastro.php" method="post">
-                <label>Descrição <input type="text" name="descricao" required></label>
-                <label>Produtos<input type="text" name="descricao" required></label>
-                <label>Preço <input type="text" name="preco" required></label>
-                <label>Adicionais <input type="text" required></label>
-                <label>Imagem <input type="file" required></label>
-                <button type="submit">Cadastrar</button>
-            </form>
-        </div>
-
-
-
-        <div class="desconto">
-        <h1>Inserir Desconto</h1>
-            <form action="../../controller/controllercadastro.php" method="post">
-                <label>ID do Produto <input type="text" name="descricao" required></label>
-                <label>Desconto<input type="text" name="descricao" required></label>
-                <button type="submit">INSERIR DESCONTO</button>
-            </form>
-        </div>
-    </div>
-
-
-
-
-
     <script src="../../js/menu.js" type="text/javascript"></script>
     <script src="../../js/noRefresh.js" type="text/javascript"></script>
 </body>
