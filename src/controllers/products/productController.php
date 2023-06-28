0866f62205def566dc8fff53b2532831623a4649
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-06-28 12:53:12 -0300

    organizei as funções e adicionei update e delete

--- src/controllers/products/productController.php
+++ src/controllers/products/productController.php
@@ -3,37 +3,41 @@ include('../../../database/conn.php');
 
 $product = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-if(isset($product['createProduto'])){
+if (isset($product['createProduto'])) {
     createProduct($product);
-} else if (isset($product['updateProduto'])){
+} else if (isset($product['updateProduct'])) {
     updateProduct($product);
-} 
+} else if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    deleteProduct($id);
+}
 
 function createProduct($product)
 {
     global $conn;
 
-    $cadastroProduto = $conn->prepare("INSERT INTO produtos (nome, descricao, preco, adicionais) VALUES (:nome, :descricao, :preco, :adicionais)");
-    $cadastroProduto->execute(array(
-    ':nome'=>$product['nome'],
-    ':descricao'=>$product['descricao'],
-    ':preco'=>$product['preco'],
-    ':adicionais'=>$product['adicionais']
-    ));
-    
-    if($cadastroProduto == true){
+    if ($product) {
+        $cadastroProduto = $conn->prepare("INSERT INTO produtos (nome, descricao, preco, adicionais)
+        VALUES (:nome, :descricao, :preco, :adicionais)");
+        $cadastroProduto->execute(
+            array(
+                ':nome' => $product['nome'],
+                ':descricao' => $product['descricao'],
+                ':preco' => $product['preco'],
+                ':adicionais' => $product['adicionais']
+            )
+        );
+
         echo ("<script>
-         alert('Produto Foi Cadastrado com Sucesso');
+       alert('Produto Foi Cadastrado com Sucesso');
+       window.location.href='../../views/admin/cadastrarProdutos.php';
+      </script>");
+    } else {
+        echo ("<script>
+         alert('ERRO: Produto NAO FOI CADASTRO');
          window.location.href='../../views/admin/cadastrarProdutos.php';
         </script>");
-    } 
-    //precisa fazer o exception para erro
-    // else {
-    //     echo ("<script>
-    //      alert('ERRO: Produto NAO FOI CADASTRO');
-    //      window.location.href='../../views/admin/cadastrarProdutos.php';
-    //     </script>");
-    // }
+    }
 
 }
 
@@ -41,35 +45,48 @@ function updateProduct($product)
 {
     global $conn;
 
-    $updateProduto = $conn->prepare("UPDATE produtos set :nome , :descricao , :preco, :adicionais WHERE id= :id");
-    $updateProduto->execute(array(
-    ':nome'=>$product['nome'],
-    ':descricao'=>$product['descricao'],
-    ':preco'=>$product['preco'],
-    ':adicionais'=>$product['adicionais'],
-    ':id'=>$product['id']
-    ));
-    
-    if($updateProduto == true){
+    if (isset($product)) {
+        $updateProduto = $conn->prepare("UPDATE produtos SET :nome , :descricao , :preco, :adicionais WHERE id = :id");
+        $updateProduto->execute(
+            array(
+                ':nome' => $product['nome'],
+                ':descricao' => $product['descricao'],
+                ':preco' => $product['preco'],
+                ':adicionais' => $product['adicionais'],
+                ':id' => $product['id']
+            )
+        );
+
         echo ("<script>
-         alert('Produto Foi Editado com Sucesso');
+        alert('Produto Foi Editado com Sucesso');
+        window.location.href='../../views/admin/cadastrarProdutos.php';
+       </script>");
+    } else {
+        echo ("<script>
+         alert('ERRO: NAO FOI ATUALIZADO');
          window.location.href='../../views/admin/cadastrarProdutos.php';
         </script>");
-    } 
-    //precisa fazer o exception para erro
-    // else {
-    //     echo ("<script>
-    //      alert('ERRO: NAO FOI ATUALIZADO');
-    //      window.location.href='../../views/admin/cadastrarProdutos.php';
-    //     </script>");
-    // }
-
+    }
 }
 
-//adicionar delete!
-function deleteProduct($product)
+function deleteProduct($id)
 {
+    global $conn;
 
+    if (isset($id)) {
+        $deleteProduct = $conn->prepare('DELETE FROM produtos WHERE id= :id');
+        $deleteProduct->execute(array(':id' => $id));
+
+        echo "<script>
+        alert('Produto Deletado!');
+        window.location.href='../../views/admin/verProdutos.php';
+        </script>";
+    } else {
+        echo "<script>
+    alert('Erro ao Deletar');
+    window.location.href='../../views/admin/verProdutos.php';
+    </script>";
+    }
 }
 
 
