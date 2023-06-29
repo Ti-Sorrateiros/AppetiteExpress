e503d4d7ea51d14f27f77aee76088176e680ca6b
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-06-29 11:53:11 -0300

    anotações e melhoria no banco de dados

--- database/sistema_de_pedidos.sql
+++ database/sistema_de_pedidos.sql
@@ -1,11 +1,17 @@
 CREATE DATABASE sistema_de_pedidos;
 USE sistema_de_pedidos;
 
+-- tabela perfil para guardar o tipo de usuario: admin  e cliente 
 CREATE TABLE perfil(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo_de_usuario VARCHAR(25) NOT NULL
+    id INT PRIMARY KEY auto_increment,
+    tipo_de_usuario VARCHAR(25)
 ) DEFAULT charset utf8;
 
+-- terá dois tipos de perfil com id: 1 para admin , 2 para cliente 
+INSERT INTO perfil (tipo_de_usuario) VALUES ('admin');
+INSERT INTO perfil (tipo_de_usuario) VALUES ('cliente');
+
+-- tabela para guardar os cadastros dos usuarios
 CREATE TABLE usuarios(
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_perfil INT, 
@@ -21,42 +27,37 @@ CREATE TABLE produtos(
     nome VARCHAR(50) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
     preco FLOAT(15) NOT NULL,
-    imagem VARCHAR(255),
-    adicionais VARCHAR(255)
+    adicionais VARCHAR(255),
+    nome_imagem VARCHAR(100), -- nome da imagem original
+    path_imagem VARCHAR(100) -- caminho da imagem 
 ) DEFAULT charset utf8; 
 
-
-CREATE TABLE pedidos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_cliente INT,
-    id_carrinho INT,
-    id_mesa INT,
-    valor_pedido FLOAT(15) NOT NULL,
-    tipo_pedido VARCHAR(55) NOT NULL,
-    data_pedido DATETIME NOT NULL
-) DEFAULT charset utf8; 
-
-CREATE TABLE mesas(
-    id INT PRIMARY KEY,
-    status VARCHAR(55) NOT NULL,
-    disponibilidade VARCHAR(25) NOT NULL
-) DEFAULT charset utf8; 
-
+-- guardar os produtos (definir limite de expiração)
 CREATE TABLE carrinho(
     id INT PRIMARY KEY,
     id_produto INT,
     quantidade_produto INT(10)
 ) DEFAULT charset utf8; 
 
+-- guardar os pedidos feito pelo cliente
+CREATE TABLE pedidos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT,
+    id_carrinho INT,
+    valor_pedido FLOAT(15) NOT NULL,
+    tipo_pedido VARCHAR(55) NOT NULL,
+    data_pedido DATETIME NOT NULL
+) DEFAULT charset utf8; 
+
+
+-- chave estrangeira id_perfil para usuario e para definir o tipo de usuario 
 ALTER TABLE usuarios ADD FOREIGN KEY(id_perfil) REFERENCES perfil (id);
 
-ALTER TABLE pedidos ADD FOREIGN KEY(id_cliente) REFERENCES usuarios (id);
+-- chave estrangeira para obter dados do usuario que pediu
+ALTER TABLE pedidos ADD FOREIGN KEY(id_usuario) REFERENCES usuarios (id);
 
-ALTER TABLE pedidos ADD FOREIGN KEY(id_carrinho) REFERENCES carrinho (id);
-
-ALTER TABLE pedidos ADD FOREIGN KEY(id_mesa) REFERENCES mesas (id);   
-
+-- chave estrangeira para obter dados do produto que foi adicionado
 ALTER TABLE carrinho ADD FOREIGN KEY(id_produto) REFERENCES produtos (id);
 
-INSERT INTO perfil (tipo_de_usuario) VALUES ('admin');
-INSERT INTO perfil (tipo_de_usuario) VALUES ('common');
+-- chave estrangeira para obter dados do carrinho para fazer o pedido
+ALTER TABLE pedidos ADD FOREIGN KEY(id_carrinho) REFERENCES carrinho (id);
