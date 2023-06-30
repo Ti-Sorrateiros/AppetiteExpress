230fcd13091cb016eb6d44d9080ecca8ef65fc2e
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-06-29 21:42:43 -0300

    formulário para editar usuário

---
+++ src/views/admin/formEdit/editarUsuario.php
@@ -0,0 +1,42 @@
+<?php
+include("../../../../database/conn.php");
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+
+$tabela = $conn->prepare("SELECT * FROM usuarios WHERE id= :id ;");
+$tabela->execute(array(':id' => $id));
+$rowTable = $tabela->fetch();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../styles/RealizarPedido.css">
+    <link rel="stylesheet" href="../../../styles/menu.css">
+    <link rel="stylesheet" href="../../../styles/GoogleFonts/GoogleFonts.css">
+    <link rel="stylesheet" href="../../../styles/cadastroProdutos.css">
+    <title>Editar Produto</title>
+</head>
+
+<body>
+    <div class="container">
+        <form method="post" action="../../../controllers/user/userController.php">
+            <input type="hidden" name="id" value='<?php echo $rowTable['id']; ?>' />
+            <label>Nome  <input type="text" name="nome" value='<?php echo $rowTable['nome']; ?>' /></label>
+            <label>Email <input type="text" name="email" value='<?php echo $rowTable['email']; ?>' /></label>
+            <label>endereco <input type="text" name="endereco" value='<?php echo $rowTable['endereco']; ?>' /></label>
+
+            <button type="submit" name="updateUser">Editar Produto</button>
+        </form>
+    </div>
+    </div>
+    <script src="../../../js/FileRead.js" type="text/javascript"></script>
+</body>
+
+</html>
