9c71032f039678069eedf18764f6f3fdddb3a9d4
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-07-03 18:22:59 -0300

    organizei  a database e add quantidade produto

--- database/sistema_de_pedidos.sql
+++ database/sistema_de_pedidos.sql
@@ -26,57 +26,7 @@ CREATE TABLE produtos(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
-    preco FLOAT(15) NOT NULL,
-    adicionais VARCHAR(255),
-    nome_imagem VARCHAR(100), -- nome da imagem original
-    path_imagem VARCHAR(100) -- caminho da imagem 
-) DEFAULT charset utf8; 
-
--- guardar os produtos (definir limite de expiração)
-CREATE TABLE carrinho(
-    id INT PRIMARY KEY,
-    id_produto INT,
-    quantidade_produto INT(10)
-) DEFAULT charset utf8; 
-
--- guardar os pedidos feito pelo cliente
-CREATE TABLE pedidos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_usuario INT,
-    id_carrinho INT,
-    valor_pedido FLOAT(15) NOT NULL,
-    tipo_pedido VARCHAR(55) NOT NULL,
-    data_pedido DATETIME NOT NULL
-) DEFAULT charset utf8; 
-
-CREATE DATABASE sistema_de_pedidos;
-USE sistema_de_pedidos;
-
--- tabela perfil para guardar o tipo de usuario: admin  e cliente 
-CREATE TABLE perfil(
-    id INT PRIMARY KEY auto_increment,
-    tipo_de_usuario VARCHAR(25)
-) DEFAULT charset utf8;
-
--- terá dois tipos de perfil com id: 1 para admin , 2 para cliente 
-INSERT INTO perfil (tipo_de_usuario) VALUES ('admin');
-INSERT INTO perfil (tipo_de_usuario) VALUES ('cliente');
-
--- tabela para guardar os cadastros dos usuarios
-CREATE TABLE usuarios(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_perfil INT, 
-    nome VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(100) NOT NULL,
-    telefone VARCHAR(11) NOT NULL UNIQUE,
-    endereco VARCHAR(50) NOT NULL
-) DEFAULT charset utf8; 
-
-CREATE TABLE produtos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(255) NOT NULL,
+    quantidade int(5),
     preco FLOAT(15) NOT NULL,
     adicionais VARCHAR(255),
     nome_imagem VARCHAR(100), -- nome da imagem original
