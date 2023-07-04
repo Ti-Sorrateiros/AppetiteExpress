351f3e8615ef96033290872641f1fc029c8fc5d5
joaoschrock <joaoschrock@gmail.com>
2023-07-03 21:33:34 -0300

    Add icons p/ ADM

--- src/views/admin/pedidosFeitos.php
+++ src/views/admin/pedidosFeitos.php
@@ -27,31 +27,31 @@
             <i class="bi bi-list" id="btn-exp"></i>
         </div>
         <ul>
-            <li class="item-menu">
-                <a href="cadastrarProdutos">
-                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
-                    <span class="txt-link"> CadastrarProdutos</span>
-                </a>
-            </li>
-            <li class="item-menu ativo">
-                <a href="pedidosFeitos">
-                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
-                    <span class="txt-link"> PedidosFeitos</span>
-                </a>
-            </li>
-            <li class="item-menu">
-                <a href="usuarios">
-                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
-                    <span class="txt-link"> Usuarios</span>
-                </a>
-            </li>
-            <li class="item-menu">
-                <a href="verprodutos">
-                    <span class="icon"><i class="bi bi-bag-fill"></i></span>
-                    <span class="txt-link">VerProdutos</span>
-                </a>
-            </li>
-        </ul>
+                <li class="item-menu ">
+                    <a href="cadastrarProdutos">
+                        <span class="icon"><i class="bi bi-bag-fill"></i></span>
+                        <span class="txt-link"> CadastrarProdutos</span>
+                    </a>
+                </li>
+                <li class="item-menu ativo">
+                    <a href="pedidosFeitos">
+                        <span class="icon"><i class="bi bi-bag-check-fill"></i></span>
+                        <span class="txt-link"> PedidosFeitos</span>
+                    </a>
+                </li>
+                <li class="item-menu">
+                    <a href="usuarios">
+                        <span class="icon"><i class="bi bi-person-circle"></i></span>
+                        <span class="txt-link"> Usuarios</span>
+                    </a>
+                </li>
+                <li class="item-menu">
+                    <a href="verprodutos">
+                        <span class="icon"><i class="bi bi-eye-fill"></i></span>
+                        <span class="txt-link">VerProdutos</span>
+                    </a>
+                </li>
+            </ul>
     </nav>
 
     <script src="../../js/menu.js" type="text/javascript"></script>
