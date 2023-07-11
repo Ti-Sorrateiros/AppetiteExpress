70c6577d141cc970499c1513c92f7fab5b2e2868
juliandersonSoares <jsoares.4720@gmail.com>
2023-07-11 16:27:13 -0300

    alteração em curso do edit de localização

--- src/views/editLocalizacao.php
+++ src/views/editLocalizacao.php
@@ -18,20 +18,29 @@ $rowTable = $select->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/cadastro.css">
+    <link rel="stylesheet" href="../styles/editLocation.css">
     <title>Edição de Localização</title>
 </head>
 
 <body>
-    <form action="../controllers/localizacaocontroller/updateController.php" method="post">
+    <div class="form">
+     <form action="../controllers/localizacaocontroller/updateController.php" method="post">
         <input type="hidden" name="id" value="<?php echo $rowTable[0]['id']; ?>" required>
-        <input type="text" id="txtCep" name="cep" value="<?php echo $rowTable[0]['cep']; ?>" required>
+        <label for="textCep">Cep</label>
+        <input type="text" id="txtCep"  type="text" name="cep" value="<?php echo $rowTable[0]['cep']; ?>" >
+        <label for="textCep">Rua</label>
         <input type="text" name="rua" id="txtrua" value="<?php echo $rowTable[0]['rua']; ?>" required />
+        <label for="textCep">Número</label>
         <input type="text" name="numero" id="txtNumero" value="<?php echo $rowTable[0]['numero']; ?>" required>
+        <label for="textCep">Bairro</label>
         <input type="text" name="bairro" id="textBairro" value="<?php echo $rowTable[0]['bairro']; ?>" required>
+        <label for="textCep">Estado</label>
         <input type="text" name="estado"  id="textEstado" value="<?php echo $rowTable[0]['estado']; ?>" required />
         <button type="submit"> Salvar </button>
     </form>
+        <button type="submit">Cadastrar</button>
+    </div>
+
 
     <script src="../js/endereco.js" type="text/javascript"></script>
 </body>
