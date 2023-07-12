f13a33381fb1b1e7530f935a8aeb382e71b58640
juliandersonSoares <jsoares.4720@gmail.com>
2023-07-11 21:41:04 -0300

    Ajustando o back-end cadastro

--- src/controllers/localizacaocontroller/cadastroController.php
+++ src/controllers/localizacaocontroller/cadastroController.php
@@ -1,18 +1,17 @@
 <?php
 include ('../../../database/conn.php');
-
+$cep = $_POST['cep'];
 $rua = $_POST['rua'];
 $numero = $_POST['numero'];
 $bairro = $_POST['bairro'];
-$cep = $_POST['cep'];
 $estado=$_POST['estado'];
 
-$local = $conn->prepare('INSERT INTO localizacao(cep,rua,numero,bairro,estado) VALUES (:rua,:numero,:bairro,:cep,:estado)');
+$local = $conn->prepare('INSERT INTO localizacao(rua,cep,numero,bairro,estado) VALUES (:rua,:cep,:numero,:bairro,:estado)');
 $local->execute(array(
     ':rua' => $rua,
+    ':cep' => $cep,
     ':numero' => $numero,
     ':bairro' => $bairro,
-    ':cep' => $cep,
     ':estado'=>$estado
     )
 );
