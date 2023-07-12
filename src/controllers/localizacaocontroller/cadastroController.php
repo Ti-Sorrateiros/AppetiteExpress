41836fbe1909bcdc029cab57acaf2a0ed6fa0b18
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-07-12 12:23:35 -0300

    cadastro de localização com o id do cliente

--- src/controllers/localizacaocontroller/cadastroController.php
+++ src/controllers/localizacaocontroller/cadastroController.php
@@ -1,18 +1,23 @@
 <?php
 include ('../../../database/conn.php');
+include('../user/protected.php');
+
+$id = $_SESSION['id'];
+
 $cep = $_POST['cep'];
 $rua = $_POST['rua'];
 $numero = $_POST['numero'];
 $bairro = $_POST['bairro'];
 $estado=$_POST['estado'];
 
-$local = $conn->prepare('INSERT INTO localizacao(rua,cep,numero,bairro,estado) VALUES (:rua,:cep,:numero,:bairro,:estado)');
+$local = $conn->prepare('INSERT INTO localizacao(id_cliente, rua,cep,numero,bairro,estado) VALUES (:id_cliente, :rua,:cep,:numero,:bairro,:estado)');
 $local->execute(array(
     ':rua' => $rua,
     ':cep' => $cep,
     ':numero' => $numero,
     ':bairro' => $bairro,
-    ':estado'=>$estado
+    ':estado'=>$estado,
+    ':id_cliente'=> $id
     )
 );
 
