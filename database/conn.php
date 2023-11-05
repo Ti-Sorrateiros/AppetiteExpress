a902602cbbde8392fd242e1685404299ecfcb2be
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-11-05 10:04:40 -0300

    configurando gitignore

--- database/conn.php
+++ database/conn.php
@@ -4,7 +4,7 @@ $host = 'localhost';
 $dbname = 'sistema_de_pedidos';
 $user = 'root';
 $pass = '';
-$port = 3307;
+$port = 3306;
 
 try{
 $conn = new PDO('mysql:host='.$host.';dbname='.$dbname.';port='.$port.';' , $user , $pass);
